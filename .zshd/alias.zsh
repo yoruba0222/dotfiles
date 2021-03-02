47cd6b658501467826791d6d623553d923d25308
Fidio-lp2 <qdatian9@gmail.com>
2021-03-02 22:59:19 +0900

    add aliases

--- .zshd/alias.zsh
+++ .zshd/alias.zsh
@@ -1,5 +1,27 @@
-alias sc='screen'
+#       _ _                     
+#  __ _| (_) __ _ ___  ___  ___ 
+# / _` | | |/ _` / __|/ _ \/ __|
+#| (_| | | | (_| \__ \  __/\__ \
+# \__,_|_|_|\__,_|___/\___||___/
+#
+
+# ls
 alias ls='exa'
+alias la='exa -a'
+alias ll='exa -l'
+alias lt='exa --tree'
+alias lta='exa -a --tree'
+
+# git commands
+alias ga='git add'
+alias gc='git commit -m'
+alias gpr='git push -u origin master'
+alias gpn='git push -u origin main'
+
+# others
+alias sc='screen'
 alias cat='bat'
 alias top='htop'
 alias c='clear ; echo -e "\e[35m$(pwd)"'
+
+
